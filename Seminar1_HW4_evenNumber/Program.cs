03725d58ebe630a92977157a5fa2ba3ca43e6895
Alena Melnichenka <6983314@mail.ru>
2023-09-06 21:36:38 +0300

    Задача на чётные числа от 1 до N

---
+++ Seminar1_HW4_evenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int numTest = int.Parse(Console.ReadLine());
+int cur = 2;
+
+while (cur < numTest)
+{
+    Console.Write($"{cur}, ");
+    cur = cur + 2;
+}

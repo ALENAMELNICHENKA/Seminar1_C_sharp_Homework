1c13e99636cfdbd0fddbe1ea45e36654c4841a5b
Alena Melnichenka <6983314@mail.ru>
2023-09-06 17:09:51 +0300

    ДЗ_1 min max$

--- Seminar1_HW1_minmax/Program.cs
+++ Seminar1_HW1_minmax/Program.cs
@@ -10,8 +10,7 @@ int num1 = int.Parse(Console.ReadLine());
 
 Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
-int max;
-int min;
+
 if (num1 > num2)
 {
     Console.WriteLine($"max: {num1}, min: {num2}");

91bc9f2002eb9b5c4d5c77b552f74a677590a57c
Alena Melnichenka <6983314@mail.ru>
2023-09-18 15:54:52 +0300

    Массив из случ. чисел с выводом на экран

---
+++ Seminar4_HW_29/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// Метод ввода числа с экрана
+int GetNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+//Метод получения случ. значений массива
+int[] GetArray(int size, int min, int max)
+{
+    int[] array = new int[size];//объявляем массив
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(min, max + 1);//заполняем случ. числами
+    }
+    return array;
+}
+
+//Метод 
+void Print(int[] arr)
+{
+    // int length = arr.Length;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    return;
+}
+ int size = GetNumber("Длина массива: ");
+ int min = GetNumber("начальный диапазон: ");
+ int max = GetNumber("Конечный диапазон: ");
+ int[] array = GetArray(size, min, max);//заполнение массива случ. числами
+ Print(array);

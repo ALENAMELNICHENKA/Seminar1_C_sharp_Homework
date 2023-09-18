8f9ae83e10b1d80980b02102f60f3cfe87d3ca4c
Alena Melnichenka <6983314@mail.ru>
2023-09-18 13:40:26 +0300

    Число А в натуральную степень В

---
+++ Seminar4_HW_25/Program.cs
@@ -0,0 +1,28 @@
+﻿// SЗадача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+// Метод читает данные от пользователя
+
+int GetNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+// Метод возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+int numberA = GetNumber("Введите число A: ");
+int numberB = GetNumber("Введите число B: ");
+ToDegree(numberA, numberB);

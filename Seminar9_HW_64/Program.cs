2ff9ab01d8c3f7164ce6a3fe271a3560d2e98e7e
Alena Melnichenka <6983314@mail.ru>
2023-10-12 15:16:41 +0300

    все натуральные числа

---
+++ Seminar9_HW_64/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// Чтение данных из консоли
+int GetNumber(string message)
+{
+    System.Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+// Печать результата
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+
+// Функция рекурсии
+string LineGenRec(int num)
+{
+    if (num == 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        return num + " " + LineGenRec(num - 1);
+    }
+}
+
+int number = GetNumber("Введите натуральное число N: ");
+string resultLine = LineGenRec(number);
+PrintResult(resultLine);
